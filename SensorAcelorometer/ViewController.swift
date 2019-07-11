2331ffcafc995b2d20afa0d1267a47551dc48737
Marilyn Martha Yusnita Devi Parhusip <marilyn.parhusip@gmail.com>
2019-07-11 16:04:42 +0700

    Initial Commit

---
+++ SensorAcelorometer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SensorAcelorometer
+//
+//  Created by Marilyn Martha Yusnita Devi Parhusip on 11/07/19.
+//  Copyright © 2019 Apple Academy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
